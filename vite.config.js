967db11644c1a7d6796173eed79990a12beea209
inessben <ine.benss@gmail.com>
2023-07-11 23:48:18 +0200

    correct online problem

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+// vite.config.js
+
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                skills: 'skills.html',
+                projects: 'projects.html'
+            }
+        }
+    }
+}
+vite.config.js
