468ed5028f1af220925ed4ba533f891a23e060dc
iness <ine.benss@gmail.com>
2025-03-27 00:51:30 +0100

    portfolio on loading

--- main/main.js
+++ main/main.js
@@ -1,5 +1,5 @@
 // imports
-import './main.css'
+// import './main.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
